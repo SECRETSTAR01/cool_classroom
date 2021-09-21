187c68df52a33fe2b8e253ebba57825e36b4baeb
Atul <tiwariatul2612@gmail.com>
2021-09-21 19:21:02 +0530

    database details updated

--- home/settings/base.py
+++ home/settings/base.py
@@ -28,7 +28,9 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'users',
     'rest_framework_simplejwt.token_blacklist',
-    'storages'
+    'storages',
+    'cloudinary_storage',
+    'cloudinary'
 ]
 
 MIDDLEWARE = [
@@ -119,12 +121,11 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-AWS_ACCESS_KEY_ID = '*******'
-AWS_SECRET_ACCESS_KEY = '******'
-AWS_STORAGE_BUCKET_NAME = '******'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_S3_REGION_NAME = 'us-west-2'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-AWS_S3_VERIFY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE={
+    'CLOUD_NAME': 'dxxtuwqwj',
+    'API_KEY': '234121729532217',
+    'API_SECRET': 'MldrbcmQbtkPxBsjnlmoo5KSumM'
+}
