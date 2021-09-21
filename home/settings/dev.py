e0f82b2498146b0a965c31ffb8d75f477e4f233f
Atul <tiwariatul2612@gmail.com>
2021-09-22 02:48:53 +0530

    database added

--- home/settings/dev.py
+++ home/settings/dev.py
@@ -7,12 +7,27 @@ DEBUG = True
 
 WSGI_APPLICATION = 'home.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "d69hv490qgcrt4",
+        "USER": "otjsxwiagrdpiq",
+        "PASSWORD": "d2afc4dba0c4c86f66f829af51d0329d06bf41e519105fc48b6ff6560fee8248",
+        "HOST": "ec2-54-158-247-97.compute-1.amazonaws.com",
+        "PORT": "5432",
     }
 }
+
+# import dj_database_url
+# dj_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(dj_from_env)
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000', 'http://127.0.0.1:8000'
--- home/settings/prod.py
+++ home/settings/prod.py
@@ -13,6 +13,11 @@ WSGI_APPLICATION = 'home.wsgi.prod.application'
 #     }
 # }
 
+# import dj_database_url
+# dj_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
